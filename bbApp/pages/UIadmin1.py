53309df2d5ede3007682eba682dbbb28dde548f3
weo1508 <143196041+weo1508@users.noreply.github.com>
2024-11-21 23:09:33 +0800

    Update UIadmin1.py

--- bbApp/pages/UIadmin1.py
+++ bbApp/pages/UIadmin1.py
@@ -34,6 +34,6 @@ st.subheader("General Function")
 join = st.button(label="Join Game", key=None, on_click=None, use_container_width=False)
 
 m.createPin(create, "UIadminWarning")
-m.nextPage(view, "UIadminView")
+m.nextPage(view, "UIadminView1")
 
 m.nextPage(join, "UIgamePin")
